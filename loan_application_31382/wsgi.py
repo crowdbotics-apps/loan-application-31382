56f6744bf3a2cd89629e63363f6bfed44f9095fd
Crowdbotics <team@crowdbotics.com>
2021-10-13 14:47:38 +0000

    Initial commit.

---
+++ loan_application_31382/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for loan_application_31382 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_application_31382.settings')
+
+application = get_wsgi_application()
